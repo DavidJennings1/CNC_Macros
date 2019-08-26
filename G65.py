51cb47b392e7569074f9d379caad930dc5b5e7fd
David Jennings <dkj.jennings@gmail.com>
2019-08-25 19:48:45 -0700

    added function

--- G65.py
+++ G65.py
@@ -16,3 +16,20 @@ class Macro():
         self.var17 = var17  # Q from macro call
         self.var3 = var3  # C from macro call
         self.var23 = var23  # W from macro call
+
+    def generate_gcode(self):
+        if var5003 >= var18:
+            ln1 = 'G0G90X{}Y{}'.format(self.var24, self.var25)
+        else:
+            ln1 = 'G0G90Z{}'.format(self.var26)
+            ln2 = 'G0G90X{}Y{}'.format(self.var24, self.var25)
+        var103 = self.var23-(self.var26+.025)
+        var104 = int(var103/(self.var17*(-1)))
+        var105 = (var103/var104)
+        var106 = (self.var2/2)
+        var107 = 1
+        ln3 = 'G1G90Z{}F200.'.format(self.var26+.025)
+        print(ln3)
+
+
+g65 = Macro(2.25, 0, 90, 5.8893, 9.647, .3182, .3182, .5, 5., .1, 0, -.0203)
